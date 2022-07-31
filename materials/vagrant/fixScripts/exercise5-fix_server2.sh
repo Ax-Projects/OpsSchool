9179ee0e4bb1740a73d0ffc8c7de57a12ab3b024
Orr Amsalem <theamsalem@gmail.com>
2022-07-31 11:57:12 +0300

    ex3-5 Done

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+sshfold=/home/vagrant/.ssh
+# sudo cp /vagrant/.vagrant/machines/server1/virtualbox/private_key /home/vagrant/.ssh/id_rsa
+# sudo chown vagrant:vagrant ~/.ssh/id_rsa
+sudo scp -o StrictHostKeyChecking=no -i $sshfold/id_rsa /vagrant/.vagrant/machines/server2/virtualbox/private_key vagrant@server1:$sshfold/id_rsa
+sudo scp -o StrictHostKeyChecking=no -i $sshfold/id_rsa vagrant@server1:$sshfold/direct-key.pub $sshfold/direct-key.pub
+cat $sshfold/direct-key.pub >> $sshfold/authorized_keys
 #add fix to exercise5-server2 here
